08ffb287c872bde2b91310a3747bacdb5bd0076c
demoraes <gabrielmoraes_2013@outlook.com>
2020-07-27 16:55:32 -0400

    add yud and configuration validation

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,12 +1,39 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    /**
+     * Validações de campos
+     */
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    /**
+     * Verifica de passou pelas validações, isValid retorna true
+     * por isso a negação com !
+     */
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    /**
+     * Verifica se o usuário já existe
+     */
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
       return res.status(400).json({ error: 'User already exists' });
     }
+
+    /**
+     * Cria o usuário no banco
+     */
 
     const { id, name, email } = await User.create(req.body);
 
@@ -18,7 +45,31 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+    /**
+     * Dados da requisição do formulario
+     */
     const { email, oldPassword } = req.body;
+
+    /**
+     * Verifica se o usuário com o respectivo id existe do banco
+     */
 
     const user = await User.findByPk(req.userId);
 
