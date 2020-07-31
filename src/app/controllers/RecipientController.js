477796f1f4b2c1364b14bfb356918def44010f72
demoraes <gabrielmoraes_2013@outlook.com>
2020-07-30 23:54:40 -0400

    add validation recipientes

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,7 +1,20 @@
+import * as Yup from 'yup';
 import Recipients from '../models/Recipients';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string().required(),
+      number: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zip_code: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
     const recipient = await Recipients.create(req.body);
 
     return res.json(recipient);
